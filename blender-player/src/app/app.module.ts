3f1144b30d3cbac497c146b44f65f09f684282ef
Nikos Deligeorgis <nikdelig@gmail.com>
2018-08-26 19:17:11 +0200

    feature(app.component): Added material sidenav

--- blender-player/src/app/app.module.ts
+++ blender-player/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { AudioPlayerComponent } from './audio-player/audio-player.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 import {HttpClientModule} from '@angular/common/http';
+import {MatSidenavModule} from '@angular/material';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -16,7 +17,9 @@ import {HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-      HttpClientModule,
+    HttpClientModule,
+    MatSidenavModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
