b545ec77d13f5d0cc1a691d5248d94077e164df7
Nikos Deligeorgis <nikdelig@gmail.com>
2019-07-28 17:57:57 +0200

    Add e2e step and fixed test

--- blender-player/e2e/app.e2e-spec.ts
+++ blender-player/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('blender-player App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Awesome Music blender');
   });
 });
